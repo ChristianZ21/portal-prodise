ecb29fa0f4aa72a766acae57c68fff3c059a8b09
Zack <151279531+ChristianZ21@users.noreply.github.com>
2026-01-24 10:57:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,15 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
+# --- OCULTAR MARCAS DE AGUA Y MENÚ ---
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 # --- TUS CLAVES (MODO SEGURO) ---
 # El sistema las buscará en la configuración de la nube, no en el código.
 try:
